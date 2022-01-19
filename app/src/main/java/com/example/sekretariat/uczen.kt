ca9b84918ef18ec66074e4912542ad891a2cfcdf
Michał Stullich <M.Stullich@student.zschie.edu.pl>
2022-01-19 22:49:50 +0100

    Dodano gui

---
+++ app/src/main/java/com/example/sekretariat/uczen.kt
@@ -0,0 +1,11 @@
+package com.example.sekretariat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class uczen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uczen)
+    }
+}
